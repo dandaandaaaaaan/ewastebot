44f2b22005ad4a6bd0a2a679a5d806d61347befc
Daniel Lee <minqiang8194@gmail.com>
2020-12-20 16:11:00 +0800

    split to use scenes

--- server.js
+++ server.js
@@ -59,21 +59,41 @@ request({
   }
 });
 
-// start scene
-const mainScene = new Scene('main');
-mainScene.enter(ctx => ctx.reply('Send your location.', Extra.markup(markup => markup.resize()
+//Functions
+function getOptions(data){
+  const names = [];
+  for(item of data){
+    names.push([item.name]);
+    names.sort();
+    return names;
+  }
+}
+
+
+//Main with /start
+bot.start((ctx) => ctx.reply("Hello \nCommands \n-/recycle - search through commonly recycled e-waste to find the nearest e-waste bins to accomodate them\n/search - find the nearest e-waste bin"));
+
+//Recycle Scene
+const recycleScene = new Scene('recycle');
+//recycleScene.enter(ctx => ctx.reply('What would you like to recycle?')
+
+
+//Search Scene
+const searchScene = new Scene('search');
+searchScene.enter(ctx => ctx.reply('Send your location.', Extra.markup(markup => markup.resize()
   .keyboard([
     markup.locationRequestButton('Send location'),
   ]))));
 
-mainScene.on('location', (ctx) => {
+searchScene.on('location', (ctx) => {
   if (data === null) {
     ctx.reply('No data');
     ctx.scene.leave();
     return;
   }
-  /** @type {Array<Outlet>} */
-  const nearestChains = data
+  /** @type {Array<EWasteBin>} */
+  const nearestBin = data
+    .filter(bin => bin.limit.items == "None")
     .map(bin => Object.assign(bin, {
       distance: geoLib.getDistance(
         ctx.message.location,
@@ -83,26 +103,23 @@ mainScene.on('location', (ctx) => {
     .sort((a, b) => {
       return a.distance - b.distance;
     });
-  if (nearestChains.length === 0) {
-    ctx.reply('No data. Enter /start to search for another store', Extra.markup((m) => m.removeKeyboard()));
+  if (nearestBin.length === 0) {
+    ctx.reply('No data. Enter /search to search for another bin', Extra.markup((m) => m.removeKeyboard()));
     ctx.scene.leave();
     return;
   }
-  ctx.reply(`Nearest Bin\n${nearestChains[0].title}\n${nearestChains[0].address}\n${nearestChains[0].distance}m`);
-  ctx.replyWithLocation(nearestChains[0].location.latitude, nearestChains[0].location.longitude);
-  ctx.reply('Enter /start to search for another store', Extra.markup((m) => m.removeKeyboard()));
+  ctx.reply(`Nearest Bin\n${nearestBin[0].title}\n${nearestBin[0].address}\n${nearestBin[0].distance}m`);
+  ctx.replyWithLocation(nearestBin[0].location.latitude, nearestBin[0].location.longitude);
+  ctx.reply('Enter /search to search for another bin', Extra.markup((m) => m.removeKeyboard()));
   ctx.scene.leave();
 });
 
 // Create scene manager
 const stage = new Stage();
 stage.command('cancel', leave());
-stage.register(mainScene);
+stage.register(searchScene);
 
 // Scene registration
 bot.use(session());
 bot.use(stage.middleware());
-bot.start((ctx) => {
-  ctx.scene.enter('main');
-});
-
+bot.command('search', (ctx) => ctx.scene.enter('search'));
